882287ae859616a3e796ee5e1f627f60bbb31d00
MichaelRosado1 <mrosad4@illinois.ed>
2020-11-18 21:46:20 -0600

    added some functions of queue class

--- queues.cpp
+++ queues.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
-
+#include <bits/stdc++.h>
+//first in first out
+//  ->stacks are first in last out
+#define MAX 10
+using namespace std;
+struct Queue {
+    int front, back;
+    int queue[MAX];
+    Queue() {
+       front = back = -1; 
+    }
+    bool isFull() {
+        // if the queue reaches from the front to the back, it is full
+        if (front == 0 && back == MAX - 1) {
+            return true;
+        }
+        return false;
+    }
+    bool isEmpty() {
+        if (front == -1) {
+            return true;
+        }
+        return false;
+    }
+}
 
 int main() {
 
