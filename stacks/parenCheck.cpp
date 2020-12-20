67940ec1db3f90a339fa2988ddea6e7642e786c4
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-20 16:39:28 -0600

    added char stack implementation

--- stacks/parenCheck.cpp
+++ stacks/parenCheck.cpp
@@ -1,6 +1,75 @@
 #include <iostream> 
+#include <string>
 
+
+#define defaultSizeOfStack 1000
+
+class Stack {
+	int stackIndex;	
+	public:
+		char items[defaultSizeOfStack];
+
+		Stack() {
+			stackIndex = -1;
+		}
+		
+		void push(char element) {
+			//if index is at more than 999, the index will go out of the array bounds
+			if (this->stackIndex >= defaultSizeOfStack - 1) {
+				std::cout<<"Stack overflow!"<<std::endl;
+			} else {
+				this->stackIndex++;
+				//sets  next index = the the given char
+				this->items[stackIndex] = element;
+			}
+
+		}
+
+		void pop() {
+			if (this->stackIndex < 0) {
+				std::cout<<"Stack underflow!"<<std::endl;
+			} else {
+				this->items[stackIndex] = ' ';
+				this->stackIndex--;
+			}	
+		}
+
+		char peek() {
+			if (stackIndex == -1) {
+				std::cout<<"stack is empty";
+				return ' ';
+			} else {
+				return this->items[stackIndex];		
+			}
+		}
+}
+
+bool checkIfStringIsEqual(std::string paren) {
+	/*
+		1) convert the string to a char vector
+		2) for each letter in the vector, check if it is an ) or (
+			if it is (, push the char to the stack
+			if it is an ), then pop the stack
+		3) if the stack is empty and there are more )'s, then return false
+			else return true
+		
+		Ex. ["(())"]
+		index 0 and 1 are pushed to the stack, then both are popped off when the char is )
+		since the stack would be empty and the char vector is empty, the parenthesis are balanced
+   */
+
+
+
+}
 
 int main() {
-
+	std::string stringToParse = "(())";
+	
+	bool isEqual = checkIfStringIsEqual(stringToParse);
+	
+	if (isEqual) {
+		std::cout<<"The inputted string is balanced correctly";
+	} else {
+		std::cout<<"The inputted string is not balanced correctly";
+	}
 }
