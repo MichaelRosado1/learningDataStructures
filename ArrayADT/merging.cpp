8c4af03abb22548fd8f69a9b04384202fc8035fd
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-24 17:32:46 -0600

    added class for arrays and other array functions

---
+++ ArrayADT/merging.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iterator>
+
+class Array {
+	int length;
+	int index;
+	int *A;
+	public:
+		Array() {
+			this->length = 10;
+			this->index = 0;
+			this->A = new int[this->length];
+		}
+
+		int getSize() {
+			return this->index;
+		}
+		int* makeABiggerCopy() {
+			int newLength = this->length * 2;
+			int originalLength = this->length;
+
+			int* toReturn = new int[newLength];
+
+			for (int i = 0; i < originalLength; i++) {
+				toReturn[i] = this->A[i];
+			}
+
+			return toReturn;
+		}
+
+		int at(int index) {
+			return this->A[index];
+		}
+
+		void print() {
+			for (int i = 0; i < this->index; i++) {
+				std::cout<<"Element: "<<i<<" is equal to: "<<this->A[i]<<"\n";
+			}
+		}
+
+		void push(int element) {
+			if (this->index == this->length) {
+				std::cout<<"Array has been resized\n";
+				this->A = makeABiggerCopy();
+				this->A[index] = element;
+				this->index++;
+			} else {
+				this->A[this->index] = element;
+				this->index++;
+			}
+		}
+};
+
+int* mergeArrays(Array *arr1, Array *arr2) {
+	int size = arr1->getSize() + arr2->getSize();
+	int *toReturn = new int[size];
+	//TODO
+	return toReturn;	
+}
+Array *myArray;
+Array *arr2;
+void printElements(int* array) { 
+	for (int i = 0; i < myArray->getSize() + arr2->getSize(); i++) {
+		std::cout<<"Element: "<<i<<" is equal to: "<<array[i]<<"\n";
+	}	
+}
+int main() {
+
+	myArray = new Array();
+	myArray->push(1);
+	myArray->push(3);
+	myArray->push(5);
+	myArray->push(7);
+	myArray->push(9);
+
+	arr2 = new Array();
+	arr2->push(2);
+	arr2->push(4);
+	arr2->push(6);
+	arr2->push(8);
+	arr2->push(10);
+
+
+	int* merged = mergeArrays(myArray, arr2);
+
+	printElements(merged);
+	
+
+}
