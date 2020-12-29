e67aad4fc1ca040ec925b3bd40da3c3f0ea0e849
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-29 15:46:13 -0600

    implemented binary search to find a given element in a dynamic array

--- ArrayADT/binarySearch.cpp
+++ ArrayADT/binarySearch.cpp
@@ -1,7 +1,38 @@
 #include <iostream> 
 
-int main() {
-	struct Array arr = {{2,3,6,3,1,3,5,7,3,2}, 10, 9};
+struct Array {
+	int a[10];
+	int length; 
+	int size;
+};
 
-	binarySearch(arr);
+void binarySearch(struct Array arr, int searchKey) {
+	int low, mid, high;
+	low = 0;
+	high = arr.length - 1;
+
+	while (low <= high) {
+		//middle of the array
+		mid = (low + high) / 2;
+
+		//if the key is at the middle of the array
+		if (searchKey == arr.a[mid]) {
+			//print the middle index
+			std::cout<<"Element("<<searchKey<<") was found at position: "<<mid;
+			//(For a sorted array) if the key is less than the middle position
+			//return to prevent infinite printing
+			return;
+		} else if (searchKey < arr.a[mid]) {
+			// move the high num down to reduce the search range
+			high = mid - 1;
+		} else {
+			// else increase the low number to reduce search range since num is above mid point
+			low = mid + 1;
+		}
+	}
+}
+int main() {
+	struct Array arr = {{1,2,3,5,7,8,22,33,23,100}, 11, 10};
+
+	binarySearch(arr,23);
 }
