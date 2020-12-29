a989c8638dca575d88bbd16c40ddc55f13bbe151
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-28 21:22:45 -0600

    added function to find the complement of a set

--- ArrayADT/setOperations.cpp
+++ ArrayADT/setOperations.cpp
@@ -121,6 +121,38 @@ struct Array *setDifference(struct Array *arr1, struct Array *arr2) {
 	return arr3;
 }
 
+/*
+   finds the complement of set arr1 
+		-> everything except elements in set a and teh intersection of set a 
+		and b
+	
+	{1,2,3}  {1,2,3,4,5,6}
+	complement -> {4,5,6}
+*/
+struct Array* compliment(struct Array* arr1, struct Array* arr2) {
+	struct Array * intersectionArr = intersection(arr1, arr2);
+	struct Array *arr3 = new Array();
+	int i,j,k;
+	i = j = k = 0;
+	//check if any elements of the intersection are in arr2
+	while (i < arr2->length && j < intersectionArr->length) {
+		if (j == intersectionArr->length - 1) {
+			arr3->a[k] = arr2->a[i];
+			k++;
+			i++;
+			j = 0;
+		}
+		if (arr2->a[i] == intersectionArr->a[j]) {
+			j = 0;
+			i++;
+		} else {
+			j++;
+		}
+	}
+	arr3->length = k;
+	arr3->size = 10;
+	return arr3;
+}
 int main() {
 	struct Array arr1 = {{1,2,3,4,5}, 10, 5};
 	
@@ -128,7 +160,7 @@ int main() {
 
 	struct Array *arr3;
 
-	arr3 = setDifference(&arr1, &arr2);
+	arr3 = compliment(&arr1, &arr2);
 
 	for (int i = 0; i < arr3->length; i++) {
 		std::cout<<arr3->a[i]<<std::endl;
