be0d7dd1e4241eb97f0af9ab3946f8063276fe9f
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-29 15:18:09 -0600

    function to find multiple missing integers in a given array

--- ArrayADT/multipleMissingElements.cpp
+++ ArrayADT/multipleMissingElements.cpp
@@ -1,6 +1,23 @@
 #include <iostream> 
 
-int main() {
+//finds multiple missing elements in an array
+void findMissingElements(int arr[], int size) {
+	int difference = arr[0];
+	//search through every element of the array 
+	for (int i = 0; i < size; i++) {
+		if (arr[i] - i != difference) {
+			while (difference < arr[i] - i) {
+				printf("%d\n", i + difference);
+				difference++;
+			}
+		}
+	}
 
+}
+
+int main() {
+	int arr[] = {6,7,8,9,11,12,15,16,17,18,19};
+	findMissingElements(arr, (sizeof(arr)/ sizeof(arr[0])));
 }	
+
 
