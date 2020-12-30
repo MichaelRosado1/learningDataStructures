efe45044177183583151f805013ead1a9d4c2010
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-29 22:22:56 -0600

    added function which reverses a c++ style string

--- strings/reverseString.cpp
+++ strings/reverseString.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <string>
+
+std::string reverseString (std::string str) {
+	std::string word = str;
+	int i, j;
+	i = 0;
+	j = word.size() - 1;
+
+	while (i < j) {
+		char temp = word[i]; 
+		word[i] = word[j];
+		word[j] = temp; 
+		i++;
+		j--;
+	}
+	return word;
+}
 
 int main() {
+	std::string otherWord = "MichaelR";
+	std::string word = "hello";
 
+	std::cout<<word<<" backwards is: "<<reverseString(word)<<std::endl;
+	std::cout<<otherWord<<" backwards is: "<<reverseString(otherWord)<<std::endl;
 }
