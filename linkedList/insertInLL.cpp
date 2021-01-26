ced0cbf1c63279e88a8482a860a5d61c613a0c50
MichaelRosado1 <mrosad4@illinois.edu>
2021-01-25 19:31:00 -0600

    created a program to insert an integer into a linked list

--- linkedList/insertInLL.cpp
+++ linkedList/insertInLL.cpp
@@ -20,7 +20,50 @@ void createLL(int a[], int n) {
 	}
 }
 
+int count(struct Node *p) {
+	struct Node *begin = new Node;
+	int count = 1;
+	for (begin = p; begin != NULL; begin = begin->nextNode) {
+		count++;	
+	}
+
+	return count;
+}
+void insert(struct Node *p, int index, int x) {
+	//not a valid position in the list
+	if (index < 0 || index > count(p)) {
+		return;
+	}
+	struct Node *t = new Node; 
+	t->data = x;	
+	
+	//if the index is the first position 
+	if (index == 0) {
+		//we have to switch the original first node with the new node, t
+		t->nextNode = first;
+		first = t;
+	} else {
+		//loop through the entire linked list
+		for (int i = 0; i < index - 1; i++) 
+			p = p->nextNode;
+		t->nextNode = p->nextNode;
+		p->nextNode = t;
+	}	
+
+
+
+}
+
 int main() {
-	int a[] = {3,4,7,19,20,8,33,2};
-	createLL(a, 8);
+	int a[] = {3,5,7};
+	createLL(a, 3);
+	insert(first,3,15);
+
+	while (first != NULL) {
+		std::cout<<first->data<<" ->";
+		first = first->nextNode;
+	}
+
+
+
 }
