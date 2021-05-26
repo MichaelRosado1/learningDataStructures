06296d0d777a1c60ddf51e2554db040e646e00b8
MichaelRosado1 <mrosad4@illinois.edu>
2021-05-25 20:59:35 -0500

    Some simple set up code before the merge function is written

--- algorithms/mergeSort.cpp
+++ algorithms/mergeSort.cpp
@@ -1,6 +1,42 @@
 #include <iostrem>
+#include <vector>
+
+using namespace std;
+
+
+void merge(vector<int>array, int l, int m, int r) {
+
+}
+
+void mergeSort(vector<int> array, int l, int r) {
+    //if the indices are equal to each other, then we have already sorted the halves
+    if (l >= r) {
+        //we should do nothing
+        return;
+    }
+
+    int median = l + (r-l) / 2
+    
+    //we want to break down the two halves
+    mergeSort(array, l, median);
+    mergeSort(array, median + 1, r);
+    
+    //we then reconstruct the array in the merge function
+    merge(array, l, m, r);
+}
 
 
 int main() {
+
+    vector<int> arr = {12,11,13,5,4,8};
+
+    int arraySize = arr.size();
+
+    //passing in the array, leftside and rightside inidices
+    mergeSort(arr, 0, arraySize - 1);
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout<<arr[i]<<" ";
+    }
 
 }
