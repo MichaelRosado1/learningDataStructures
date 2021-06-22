59b5a138f3f34b17ebee7722dbba8c0be0654c3a
MichaelRosado1 <mrosad4@illinois.edu>
2021-06-22 12:38:34 -0500

    implemented printing of the array to view the sort

--- algorithms/countingSort.cpp
+++ algorithms/countingSort.cpp
@@ -1,1 +1,33 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+void print(vector<int> array) {
+    cout<<"{ ";
+    for (int i = 0; i < array.size(); i++) {
+        cout<<array[i];
+        if (i == array.size() - 1) { 
+            cout<<" ";
+        } else {
+            cout<<" , ";
+        }
+    }
+    cout<<" } ";
+}
+
+
+
+int main() {
+    //initialize a vector of arbitrary integers
+    vector<int> arr;
+    arr.push_back(29);
+    arr.push_back(14);
+    arr.push_back(1);
+    arr.push_back(9);
+    arr.push_back(18);
+    arr.push_back(89);
+
+    print(arr);
+
+}
 
